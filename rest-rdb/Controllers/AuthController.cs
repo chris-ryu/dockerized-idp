42907d6973c6516b06d1021aa8c5d6f85e8a1117
Chris Ryu <narzis@gmail.com>
2020-06-25 14:00:26 +0900

    Oracle DB 연결

--- rest-rdb/Controllers/AuthController.cs
+++ rest-rdb/Controllers/AuthController.cs
@@ -30,8 +30,8 @@ namespace webapi.Controllers {
         [HttpGet ("attributes/{userId}")]
         public async Task<IActionResult> GetAttributes (int userId) {
             _logger.LogInformation ("Get Method Log");
-            // var auth_query = Environment.GetEnvironmentVariable ("AUTH_QUERY");
-            string auth_query = $"SELECT ID AS \"userId\", USERNAME AS \"userName\", FIRST_NAME AS \"givenName\", LAST_NAME AS \"sn\", EMAIL AS \"mail\" FROM DBUSER.USERS WHERE ID=:userId";
+            var auth_query = Environment.GetEnvironmentVariable ("AUTH_QUERY");
+            // string auth_query = $"SELECT ID AS \"userId\", USERNAME AS \"userName\", FIRST_NAME AS \"givenName\", LAST_NAME AS \"sn\", EMAIL AS \"mail\" FROM DBUSER.USERS WHERE ID=:userId";
             // var constring = Environment.GetEnvironmentVariable("CONNECTION_STRING");
             var server = Environment.GetEnvironmentVariable ("HOST");
             var port = Environment.GetEnvironmentVariable ("DB_PORT");
@@ -40,8 +40,8 @@ namespace webapi.Controllers {
             var dbpassword = Environment.GetEnvironmentVariable ("PASSWORD");
             //
             dynamic expando = new ExpandoObject();
-            // string conString = $"Data Source={server}:{port}/{database};User Id={userName};Password={dbpassword}";
-            string conString = $"Data Source=13.209.18.89:32118/XEPDB1;User Id=dbuser;Password=dbpassword";
+            string conString = $"Data Source={server}:{port}/{database};User Id={userName};Password={dbpassword}";
+            // string conString = $"Data Source=13.209.18.89:32118/XEPDB1;User Id=dbuser;Password=dbpassword";
             _logger.LogInformation (conString);
             try {
                 // using (var conn = new DB2Connection ("Server=ifx:9089;Database=stores_demo;UID=informix;Password=in4mix")) {
@@ -88,7 +88,7 @@ namespace webapi.Controllers {
             }
             _logger.LogInformation ("Get Method Log");
             // var auth_query = Environment.GetEnvironmentVariable ("AUTH_QUERY");
-            string auth_query = $"SELECT ID AS \"userId\", USERNAME AS \"userName\", FIRST_NAME AS \"givenName\", LAST_NAME AS \"sn\", EMAIL AS \"mail\" FROM DBUSER.USERS WHERE USERNAME=:username AND PASSWORD=DBMS_CRYPTO.HASH(UTL_I18N.STRING_TO_RAW(:password, 'AL32UTF8'), 4)";
+            string auth_query = $"SELECT ID AS \"uid\", USERNAME AS \"userName\", FIRST_NAME AS \"givenName\", LAST_NAME AS \"sn\", EMAIL AS \"mail\" FROM DBUSER.USERS WHERE USERNAME=:username AND PASSWORD=DBMS_CRYPTO.HASH(UTL_I18N.STRING_TO_RAW(:password, 'AL32UTF8'), 4)";
             // var constring = Environment.GetEnvironmentVariable("CONNECTION_STRING");
             var server = Environment.GetEnvironmentVariable ("HOST");
             var port = Environment.GetEnvironmentVariable ("DB_PORT");
